3ede872e5ea64c5a54e28f2bfad5f82ea9465c42
Kia Farhang <kfarhang0@gmail.com>
2018-02-13 16:22:24 -0600

    Add class for Attributes, top-level numbers that determine battle stats

---
+++ src/main/java/Attributes.java
@@ -0,0 +1,29 @@
+public class Attributes {
+    private int strength;
+    private int dexterity;
+    private int constitution;
+    private int intelligence;
+
+    public Attributes(int str, int dex, int con, int intel) {
+        this.strength = str;
+        this.dexterity = dex;
+        this.constitution = con;
+        this.intelligence = intel;
+    }
+
+    public int getStrength() {
+        return this.strength;
+    }
+
+    public int getDexterity() {
+        return this.dexterity;
+    }
+
+    public int getConstitution() {
+        return this.constitution;
+    }
+
+    public int getIntelligence() {
+        return this.intelligence;
+    }
+}
