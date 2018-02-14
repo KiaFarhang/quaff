926d4afecbe4f97840576d46b8abed19b760ded3
Kia Farhang <kfarhang0@gmail.com>
2018-02-14 14:14:26 -0600

    Create basic Warrior class

---
+++ src/main/java/Warrior.java
@@ -0,0 +1,19 @@
+public class Warrior implements HasBattleStats {
+    private Attributes attributes;
+    private BattleStats maxStats;
+    private BattleStats currentStats;
+
+    public Warrior() {
+        this.attributes = new Attributes(8, 5, 5, 2);
+        this.maxStats = new BattleStats(5, 8, 5, 5, 2);
+        this.currentStats = new BattleStats(5, 8, 5, 5, 2);
+    }
+
+    public BattleStats getMaxStats() {
+        return this.maxStats;
+    }
+
+    public BattleStats getCurrentStats() {
+        return this.currentStats;
+    }
+}
