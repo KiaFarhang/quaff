f20609cddad69ce112187dd1c5834d6662b8b3bf
Kia Farhang <kfarhang0@gmail.com>
2018-02-15 15:05:18 -0600

    Create a separate directory for battle-related code

---
+++ src/main/java/com/kiafarhang/quaff/battle/AttackFormula.java
@@ -0,0 +1,26 @@
+package com.kiafarhang.quaff.battle;
+
+import java.util.*;
+import com.kiafarhang.quaff.*;
+
+public class AttackFormula {
+    public static int calculateDamage(HasBattleStats attacker, HasBattleStats target) {
+        BattleStats attackerStats = attacker.getCurrentStats();
+        BattleStats targetStats = target.getCurrentStats();
+
+        Random random = new Random();
+
+        int attackerATK = attackerStats.getATK();
+        int targetDEF = targetStats.getDEF();
+
+        int critChance = attackerStats.getLUK() - 1;
+
+        if (critChance >= random.nextInt()) {
+            System.out.println("Critical hit!");
+            attackerATK *= 2;
+        }
+
+        return attackerATK - targetDEF;
+
+    }
+}
