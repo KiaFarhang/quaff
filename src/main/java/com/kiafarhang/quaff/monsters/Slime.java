fb23f26a78b9a83ea482f1e02cd137ff2b84c78d
Kia Farhang <kfarhang0@gmail.com>
2018-02-18 10:33:37 -0600

    Started directory for monsters, added a Slime monster class

---
+++ src/main/java/com/kiafarhang/quaff/monsters/Slime.java
@@ -0,0 +1,22 @@
+package com.kiafarhang.quaff.monsters;
+
+import com.kiafarhang.quaff.BattleStats;
+import com.kiafarhang.quaff.HasBattleStats;
+
+public class Slime implements HasBattleStats {
+    private BattleStats maxStats;
+    private BattleStats currentStats;
+
+    public Slime() {
+        this.maxStats = new BattleStats(2, 2, 2, 4, 1);
+        this.currentStats = new BattleStats(2, 2, 2, 4, 1);
+    }
+
+    public BattleStats getMaxStats() {
+        return this.maxStats;
+    }
+
+    public BattleStats getCurrentStats() {
+        return this.currentStats;
+    }
+}
