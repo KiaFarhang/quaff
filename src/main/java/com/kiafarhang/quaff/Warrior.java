eaeacdd61e8a898a95c3e1785c2f34c985232a10
Kia Farhang <kfarhang0@gmail.com>
2018-02-19 13:29:22 -0600

    Give the Warrior class a bit less agility

--- src/main/java/com/kiafarhang/quaff/Warrior.java
+++ src/main/java/com/kiafarhang/quaff/Warrior.java
@@ -7,8 +7,8 @@ public class Warrior implements HasBattleStats {
 
     public Warrior() {
         this.attributes = new Attributes(8, 5, 5, 2);
-        this.maxStats = new BattleStats(5, 8, 5, 5, 2);
-        this.currentStats = new BattleStats(5, 8, 5, 5, 2);
+        this.maxStats = new BattleStats(5, 8, 5, 3, 2);
+        this.currentStats = new BattleStats(5, 8, 5, 3, 2);
     }
 
     public BattleStats getMaxStats() {
